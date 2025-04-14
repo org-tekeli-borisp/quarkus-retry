c65925fde2974d127c0e4e83970a48baafa08bfd
borisp <boris.pigin@gmail.com>
2025-04-14 23:10:26 +0200

    Add specification and consumption logic for Kafka integration
    Implemented a Quarkus specification to validate Kafka consumer behavior, ensuring temperature measurements are correctly processed and stored. Introduced the `@Incoming` annotation for seamless message consumption and configured application properties for Kafka messaging. Added necessary dependencies, including Awaitility, to support asynchronous specifications.

--- src/main/kotlin/org/tekeli/borisp/TemperatureMeasurementKafkaConsumer.kt
+++ src/main/kotlin/org/tekeli/borisp/TemperatureMeasurementKafkaConsumer.kt
@@ -4,6 +4,7 @@ import io.quarkus.logging.Log
 import jakarta.enterprise.context.Dependent
 import jakarta.inject.Inject
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.eclipse.microprofile.reactive.messaging.Incoming
 
 @Dependent
 class TemperatureMeasurementKafkaConsumer() {
@@ -11,6 +12,7 @@ class TemperatureMeasurementKafkaConsumer() {
     @Inject
     lateinit var temperatureMeasurementService: TemperatureMeasurementService
 
+    @Incoming("temperature-measurements")
     fun consume(consumerRecord: ConsumerRecord<String, TemperatureMeasurement>) {
         Log.info("Consuming record: $consumerRecord")
         temperatureMeasurementService.save(consumerRecord.value())
--- src/test/kotlin/org/tekeli/borisp/Givens.kt
+++ src/test/kotlin/org/tekeli/borisp/Givens.kt
@@ -4,7 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
 import java.time.OffsetDateTime
+import java.util.*
 
 fun givenConsumerRecord(): ConsumerRecord<String, TemperatureMeasurement> {
     val topic = "topic"
@@ -34,3 +39,14 @@ fun ObjectMapperDeserializer<TemperatureMeasurement>.registerJavaTimeModule() {
     objectMapper.registerModule(JavaTimeModule())
     declaredField.isAccessible = false
 }
+
+fun givenTestKafkaProducer(bootstrapServers: String): KafkaProducer<String, String> {
+    val config = Properties()
+    config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+    config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.canonicalName
+    config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.canonicalName
+    return KafkaProducer<String, String>(config)
+}
+
+fun givenProducerRecord(topic: String): ProducerRecord<String, String> =
+    ProducerRecord(topic, null, "Hamburg", givenTemperatureMeasurementAsJson())
