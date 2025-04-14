7702eb4b8bfa59a999d96d41c905e9ee7c83a75b
borisp <boris.pigin@gmail.com>
2025-04-14 23:16:48 +0200

    Refactor dependency injection in Kafka consumer constructor
    Changed dependency injection to constructor-based for `TemperatureMeasurementService` in `TemperatureMeasurementKafkaConsumer`. This improves clarity, simplifies initialization, and aligns with specification-driven development principles.

--- src/main/kotlin/org/tekeli/borisp/TemperatureMeasurementKafkaConsumer.kt
+++ src/main/kotlin/org/tekeli/borisp/TemperatureMeasurementKafkaConsumer.kt
@@ -2,16 +2,11 @@ package org.tekeli.borisp
 
 import io.quarkus.logging.Log
 import jakarta.enterprise.context.Dependent
-import jakarta.inject.Inject
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.eclipse.microprofile.reactive.messaging.Incoming
 
 @Dependent
-class TemperatureMeasurementKafkaConsumer() {
-
-    @Inject
-    lateinit var temperatureMeasurementService: TemperatureMeasurementService
-
+class TemperatureMeasurementKafkaConsumer(val temperatureMeasurementService: TemperatureMeasurementService) {
     @Incoming("temperature-measurements")
     fun consume(consumerRecord: ConsumerRecord<String, TemperatureMeasurement>) {
         Log.info("Consuming record: $consumerRecord")
