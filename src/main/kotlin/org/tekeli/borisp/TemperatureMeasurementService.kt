861bb43fb0e875f7c44e396f99e7ff5b3427ab71
borisp <boris.pigin@gmail.com>
2025-04-14 17:49:01 +0200

    Add TemperatureMeasurementService specifications and persistence logic
    Introduced specifications for the `TemperatureMeasurementService` to validate its behavior. Implemented in-memory storage for temperature measurements, along with methods to save and retrieve all measurements.

--- src/main/kotlin/org/tekeli/borisp/TemperatureMeasurementService.kt
+++ src/main/kotlin/org/tekeli/borisp/TemperatureMeasurementService.kt
@@ -5,7 +5,15 @@ import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class TemperatureMeasurementService() {
+
+    private val temperatureMeasurements = mutableListOf<TemperatureMeasurement>()
+
     fun save(temperatureMeasurement: TemperatureMeasurement) {
         Log.info("Saving temperature measurement $temperatureMeasurement")
+        temperatureMeasurements.add(temperatureMeasurement)
+    }
+
+    fun getAll(): List<TemperatureMeasurement> {
+        return temperatureMeasurements
     }
 }
---
+++ src/test/kotlin/org/tekeli/borisp/TemperatureMeasurementServiceTest.kt
@@ -0,0 +1,45 @@
+package org.tekeli.borisp
+
+import io.quarkus.test.component.QuarkusComponentTest
+import jakarta.inject.Inject
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+@QuarkusComponentTest
+class TemperatureMeasurementServiceTest {
+
+    @Inject
+    lateinit var temperatureMeasurementService: TemperatureMeasurementService
+
+    @Test
+    fun `first saving of one TemperatureMeasurement leads to list size of one`() {
+        assertThat(temperatureMeasurementService.getAll()).hasSize(0)
+
+        temperatureMeasurementService.save(TemperatureMeasurement())
+
+        assertThat(temperatureMeasurementService.getAll()).hasSize(1)
+    }
+
+    @Test
+    fun `two savings of two different TemperatureMeasurement's leads to list size of two`() {
+        val temperatureMeasurement1 = TemperatureMeasurement()
+        val temperatureMeasurement2 = TemperatureMeasurement()
+        assertThat(temperatureMeasurementService.getAll()).hasSize(0)
+
+        temperatureMeasurementService.save(temperatureMeasurement1)
+        temperatureMeasurementService.save(temperatureMeasurement2)
+
+        assertThat(temperatureMeasurementService.getAll()).hasSize(2)
+    }
+
+    @Test
+    fun `two savings of the same TemperatureMeasurement's leads to list size of two`() {
+        val temperatureMeasurement = TemperatureMeasurement()
+        assertThat(temperatureMeasurementService.getAll()).hasSize(0)
+
+        temperatureMeasurementService.save(temperatureMeasurement)
+        temperatureMeasurementService.save(temperatureMeasurement)
+
+        assertThat(temperatureMeasurementService.getAll()).hasSize(2)
+    }
+}
